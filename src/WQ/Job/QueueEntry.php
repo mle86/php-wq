8dad1ebe8aa93ce34be7fbdf022ee8f569102cac
mle <maximilian@eul.cc>
2017-04-19 13:14:41 +0200

    clarify $jobId doc

--- src/WQ/Job/QueueEntry.php
+++ src/WQ/Job/QueueEntry.php
@@ -53,7 +53,7 @@ final class QueueEntry
 	 * @param string $serializedData  The serialized raw data.
 	 * @param string $originWorkQueue
 	 * @param mixed $handle  The Work Server adapter's representation of this job.
-	 * @param string $jobId  A unique ID for this job. Only used for logging.
+	 * @param string $jobId  A unique ID for this job. Only used for logging. Not every WorkServer implementation provides this!
 	 * @return QueueEntry
 	 * @throws UnserializationException  if $serializedData corresponded to a non-object or to a non-{@see Job} object
 	 */
